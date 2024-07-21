9ad32ca3be09c23b850240c5dd812ff3668a62a9
Micoo98 <37417540+Micoo98@users.noreply.github.com>
2024-07-20 22:19:42 -0400

    Got flipping working

--- functions.js
+++ functions.js
@@ -1,7 +1,18 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
     function toggleImage(image) {
-        image.src = "./Assets/Flipped.jpg";
+        if(image.getAttribute('data-flipped') === 'false' || image.getAttribute('data-flipped') === null)
+        {
+            image.setAttribute('original-image', image.src);
+            image.src = "./Assets/Flipped.jpg";
+            image.setAttribute('data-flipped', true);
+        }
+        else
+        {
+            // alert("it did the else");
+            image.src = image.getAttribute('original-image');
+            image.setAttribute('data-flipped', false);
+        }
     }
 
     const flippables = document.getElementsByClassName('flippable');
